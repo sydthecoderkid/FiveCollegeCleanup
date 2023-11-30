43aa36df0b2ce82c67fcebcf10472694769e3e40
Cuong <vnguyen24@amherst.edu>
2023-11-30 18:58:05 -0500

    Added other routes

--- 5college/src/App.js
+++ 5college/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Students from './Students';
+import Instructors from './Instructors';
+import Courses from './Courses';
+import Meetings from './Meetings';
 import LandingPage from './LandingPage';
 
 const App = () => {
@@ -9,6 +12,9 @@ const App = () => {
 			<Routes>
 				<Route path='/' element={<LandingPage />} />
 				<Route path='/students' element={<Students />} />
+				<Route path='/instructors' element={<Instructors />} />
+				<Route path='/courses' element={<Courses />} />
+				<Route path='/meetings' element={<Meetings />} />
 			</Routes>
 		</Router>
 	);
---
+++ 5college/src/Meetings.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Meetings = () => {
+	return <h1>Meetings Page</h1>;
+};
+
+export default Meetings;
