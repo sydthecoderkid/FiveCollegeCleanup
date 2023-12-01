d35a0aa3152fc117a1e5dab6c5b2d8af696927b2
sydthecoderkid <bookmasterdc@gmail.com>
2023-11-30 19:11:45 -0500

    remove hashtag

--- 5college/src/App.js
+++ 5college/src/App.js
@@ -7,27 +7,27 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 const App = () => {
-	return (
-		<Router>
+  return (
+    <Router>
       <Navbar>
-       <Container>
-         {/* <Navbar.Brand href="#home">Home</Navbar.Brand> */}
-           <Nav className="me-auto">
-             <Nav.Link href="#home">Home</Nav.Link>
-             <Nav.Link href="students">Students</Nav.Link>
-             <Nav.Link href="#instructors">Instructors</Nav.Link>
-             <Nav.Link href="#courses">Courses</Nav.Link>
-             <Nav.Link href="#meetings">Meetings</Nav.Link>
-         </Nav>
-       </Container>
-     </Navbar>
+        <Container>
+          {/* <Navbar.Brand href="#home">Home</Navbar.Brand> */}
+          <Nav className="me-auto">
+            <Nav.Link href="home">Home</Nav.Link>
+            <Nav.Link href="students">Students</Nav.Link>
+            <Nav.Link href="instructors">Instructors</Nav.Link>
+            <Nav.Link href="courses">Courses</Nav.Link>
+            <Nav.Link href="meetings">Meetings</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
-			<Routes>
-				<Route path='/' element={<LandingPage />} />
-				<Route path='/students' element={<Students />} />
-			</Routes>
-		</Router>
-	);
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/students' element={<Students />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
