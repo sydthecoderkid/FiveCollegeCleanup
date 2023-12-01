cd2c873267513e4a0fdc56f33d1dcef696eadf88
Cuong <vnguyen24@amherst.edu>
2023-11-30 19:14:11 -0500

    fixed package.json + routing

--- 5college/src/App.js
+++ 5college/src/App.js
@@ -12,18 +12,18 @@ import Navbar from 'react-bootstrap/Navbar';
 const App = () => {
 	return (
 		<Router>
-      <Navbar>
-       <Container>
-         {/* <Navbar.Brand href="#home">Home</Navbar.Brand> */}
-           <Nav className="me-auto">
-             <Nav.Link href="#home">Home</Nav.Link>
-             <Nav.Link href="students">Students</Nav.Link>
-             <Nav.Link href="#instructors">Instructors</Nav.Link>
-             <Nav.Link href="#courses">Courses</Nav.Link>
-             <Nav.Link href="#meetings">Meetings</Nav.Link>
-         </Nav>
-       </Container>
-     </Navbar>
+			<Navbar>
+				<Container>
+					{/* <Navbar.Brand href="#home">Home</Navbar.Brand> */}
+					<Nav className='me-auto'>
+						<Nav.Link href='/'>Home</Nav.Link>
+						<Nav.Link href='students'>Students</Nav.Link>
+						<Nav.Link href='instructors'>Instructors</Nav.Link>
+						<Nav.Link href='courses'>Courses</Nav.Link>
+						<Nav.Link href='meetings'>Meetings</Nav.Link>
+					</Nav>
+				</Container>
+			</Navbar>
 
 			<Routes>
 				<Route path='/' element={<LandingPage />} />
